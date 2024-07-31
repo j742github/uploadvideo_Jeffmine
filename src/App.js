e3bebdfffde3ff3d6bf46c555a59992aac00cb89
j742github <j247yang@uwaterloo.ca>
2024-07-31 07:38:03 -0400

    final page outline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
 
-
-{/*uploadpage5.js nihao123*/}
-
 function App() {
   return (
     <div className="upload-page">
@@ -24,135 +21,33 @@ function App() {
         </div>
       </header>
 
-{/* ----------------------- */}
-
-{/*video containers*/}
-      <div className="main-content">
-        <div className="profile-section">
-          <div className="profile-info">
-            <div className="profile-pic-large">
-              <img src={"profile-icon.svg"} alt="profile" />
-            </div>
-            <div className="profile-details">
-              <div className="profile-name">Raven</div>
-              <button className="edit-profile">Edit Profile</button>
-            </div>
-          </div>
+      <div className="video-player-container">
+        <div className="video-player">
+          <img src={"random-video.jpg"} alt="Video" />
         </div>
-
-
-
-        <div className="collections">
-          <div className="collection">
-            <img src={"fullbody.jpg"} alt="Weight Loss Challenge" />
-            <div className="collection-title">28-Days Intermittent Fasting Weight Loss Challenge</div>
-            <div className="collection-info">Tom | 1 hour ago | 2 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"laugh.jpg"} alt="Try Not to Laugh Challenge" />
-            <div className="collection-title">Try Not to Laugh Challenge</div>
-            <div className="collection-info">Raven | 2 weeks ago | 22 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"abworkout.jpg"} alt="Ab Workout Challenge!" />
-            <div className="collection-title">Ab Workout Challenge!</div>
-            <div className="collection-info">Jason | 1 month ago | 80 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"fullbodyworkout.jpg"} alt="Best Full Body Workout to Lose Fat" />
-            <div className="collection-title">Best Full Body Workout to Lose Fat . 20 mins | 28 Day Challenge</div>
-            <div className="collection-info">Raven | 2 months ago | 160 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"trynottolaugh.jpg"} alt="Try not to cry laughing" />
-            <div className="collection-title">Try not to cry laughing</div>
-            <div className="collection-info">Justin | 2 months ago | 223 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"raisingcanes.jpg"} alt="Raising Cane's Full Menu Challenge" />
-            <div className="collection-title">Raising Cane's Full Menu Challenge!</div>
-            <div className="collection-info">Raven | 3 months ago | 550 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"speeddrawing.jpg"} alt="Speed Drawing Challenge" />
-            <div className="collection-title">Speed Drawing Challenge!</div>
-            <div className="collection-info">Naomi | 2 months ago | 129 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"nocarbs.jpg"} alt="NO SUGAR OR CARBS FOR A WEEK" />
-            <div className="collection-title">NO SUGAR OR CARBS FOR A WEEK?</div>
-            <div className="collection-info">Raven | 4 months ago | 240 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"spicygolgappa.jpg"} alt="World's Spiciest Golgappa Challenge" />
-            <div className="collection-title">World's Spiciest Golgappa Challenge</div>
-            <div className="collection-info">David | 1 year ago | 992 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"clouddatachallenge.jpg"} alt="Complete Solution For Learn to Earn Cloud Data Challenge" />
-            <div className="collection-title">Complete Solution For Learn to Earn Cloud Data Challenge</div>
-            <div className="collection-info">Naomi | 1 year ago | 787 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"workout.jpg"} alt="5 Hour Workout Challenge" />
-            <div className="collection-title">5 Hour Workout Challenge</div>
-            <div className="collection-info">Raven | 2 years ago | 590 Views</div>
-          </div>
-          <div className="collection">
-            <img src={"yoga.jpg"} alt="Extreme Yoga Challenge" />
-            <div className="collection-title">Extreme Yoga Challenge</div>
-            <div className="collection-info">Raven | 2 years ago | 768 Views</div>
+        <div className="video-details">
+          <h2 className="video-title">Awesome Workout Video</h2>
+          <p className="video-description">Check out this amazing workout routine!</p>
+          <div className="uploaded-by">Uploaded by Raven • 2 months ago</div>
+          <div className="collection-info">Collection: Diet</div>
+          <div className="actions">
+            <button className="like-button">Like</button>
+            <button className="approve-button">Approve</button>
           </div>
         </div>
       </div>
-{/*end of video containers */}
 
+      <div className="comment-section">
+        {/* Comment section */}
+        <div className="comment">User123: Great video!</div>
+        <div className="comment">User456: I love this workout!</div>
+      </div>
 
-{/* bottom bar */}
       <footer className="footer">
-        <div><b>Top Witnesses</b></div>
-        <div className="witlist">
-        <div className="witness-item">1 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Raven</div>
-          <div className="witness-item">2 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Tom</div>
-          <div className="witness-item">3 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Jack</div>
-          <div className="witness-item">4 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Jason</div>
-          <div className="witness-item">5 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Naomi</div>
-        </div>
-        <div>
-                    <a href="/" style={{textDecoration: 'none'}} className="view-all">View All</a>
-        </div>
+        {/* Bottom bar */}
       </footer>
-{/* end of bottom bar */}
-
-
-{/* bottom bar */}
-<footer className="footer">
-        <div><b>Top Witnesses</b></div>
-        <div className="witlist">
-        <div className="witness-item">1 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Olivia</div>
-          <div className="witness-item">2 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Mia</div>
-          <div className="witness-item">3 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Sophia</div>
-          <div className="witness-item">4 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>David</div>
-          <div className="witness-item">5 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Luca</div>
-        </div>
-        <div>
-                    <a href="/" style={{textDecoration: 'none'}} className="view-all">View All</a>
-        </div>
-      </footer>
-{/* end of bottom bar */}
-
-
-
-
-
-
-
-
     </div>
-   
   );
 }
-
-{/* TESTING FOR GITHUB */}
 
 export default App;
---
+++ src/uploadpage5.js
@@ -0,0 +1,170 @@
+import './App.css';
+
+
+{/*uploadpage5.js nihao123*/}
+
+function App() {
+  return (
+    <div className="upload-page">
+      <header className="header">
+        <div className="left-side">
+          <div className="profile-pic">
+            <img src={"profile-icon.svg"} alt="icon" />
+          </div>
+          <div className="camera-icon">
+            <img src={"add-video.svg"} alt="add-video-icon" />
+          </div>
+        </div>
+        <div className="search-bar">
+        <div className="search-icon"><img src={"search-icon.svg"} alt="search-icon" /></div>
+          <input className="search-box" type="text" placeholder="Search Challenge Collection" />
+        </div>
+        <div className="wallet-button">
+          <button className="Connect-wallet">Connect Wallet</button>
+        </div>
+      </header>
+
+{/* ----------------------- */}
+
+{/*video containers*/}
+      <div className="main-content">
+        <div className="profile-section">
+          <div className="profile-info">
+            <div className="profile-pic-large">
+              <img src={"profile-icon.svg"} alt="profile" />
+            </div>
+            <div className="profile-details">
+              <div className="profile-name">Raven</div>
+              <button className="edit-profile">Edit Profile</button>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div className="collections">
+          <div className="collection">
+            <img src={"fullbody.jpg"} alt="Weight Loss Challenge" />
+            <div className="collection-title">28-Days Weight Loss Challenge</div>
+            <div className="collection-info">
+              Tom | 1 hour ago | 2 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"laugh.jpg"} alt="Try Not to Laugh Challenge" />
+            <div className="collection-title">Try Not to Laugh Challenge</div>
+            <div className="collection-info">Raven | 2 weeks ago | 22 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"abworkout.jpg"} alt="Ab Workout Challenge!" />
+            <div className="collection-title">Ab Workout Challenge!</div>
+            <div className="collection-info">Jason | 1 month ago | 80 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"fullbodyworkout.jpg"} alt="Best Full Body Workout to Lose Fat" />
+            <div className="collection-title">28 Day Workout to Lose Fat Challenge</div>
+            <div className="collection-info">Raven | 2 months ago | 160 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"trynottolaugh.jpg"} alt="Try not to cry laughing" />
+            <div className="collection-title">Try not to cry laughing</div>
+            <div className="collection-info">Justin | 2 months ago | 223 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"raisingcanes.jpg"} alt="Raising Cane's Full Menu Challenge" />
+            <div className="collection-title">Raising Cane's Full Menu Challenge!</div>
+            <div className="collection-info">Raven | 3 months ago | 550 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"speeddrawing.jpg"} alt="Speed Drawing Challenge" />
+            <div className="collection-title">Speed Drawing Challenge!</div>
+            <div className="collection-info">Naomi | 2 months ago | 129 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"nocarbs.jpg"} alt="NO SUGAR OR CARBS FOR A WEEK" />
+            <div className="collection-title">NO SUGAR OR CARBS FOR A WEEK?</div>
+            <div className="collection-info">Raven | 4 months ago | 240 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"spicygolgappa.jpg"} alt="World's Spiciest Golgappa Challenge" />
+            <div className="collection-title">World's Spiciest Golgappa Challenge</div>
+            <div className="collection-info">David | 1 year ago | 992 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"clouddatachallenge.jpg"} alt="Learn to Earn Cloud Data" />
+            <div className="collection-title">Learn to Earn Cloud Data</div>
+            <div className="collection-info">Naomi | 1 year ago | 787 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"workout.jpg"} alt="5 Hour Workout Challenge" />
+            <div className="collection-title">5 Hour Workout Challenge</div>
+            <div className="collection-info">Raven | 2 years ago | 590 Views</div>
+          </div>
+          <div className="collection">
+            <img src={"yoga.jpg"} alt="Extreme Yoga Challenge" />
+            <div className="collection-title">Extreme Yoga Challenge</div>
+            <div className="collection-info">Raven | 2 years ago | 768 Views</div>
+          </div>
+        </div>
+      </div>
+{/*end of video containers */}
+
+{/*pagination*/}
+<div className="pagination" style={{ float: 'right', marginRight: '100px' }}>
+<a href="#">1</a>
+  <span style={{ margin: '0 5px' }}></span>
+  <a href="#">2</a>
+  <span style={{ margin: '0 5px' }}></span>
+  <a href="#">3</a>
+  <span style={{ margin: '0 5px' }}></span>
+  <button style={{ backgroundColor: '#f0f0f0', border: '1px solid #ccc' }}>&gt;</button>
+</div>
+{/*end of pagination*/}
+
+{/* bottom bar */}
+      <footer className="footer">
+        <div><b>Top Witnesses</b></div>
+        <div className="witlist">
+        <div className="witness-item">1 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Raven</div>
+          <div className="witness-item">2 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Tom</div>
+          <div className="witness-item">3 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Jack</div>
+          <div className="witness-item">4 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Jason</div>
+          <div className="witness-item">5 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Naomi</div>
+        </div>
+        <div>
+                    <a href="/" style={{textDecoration: 'none'}} className="view-all">View All</a>
+        </div>
+      </footer>
+{/* end of bottom bar */}
+
+
+{/* bottom bar */}
+<footer className="footer">
+        <div><b>Top Witnesses</b></div>
+        <div className="witlist">
+        <div className="witness-item">1 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Olivia</div>
+          <div className="witness-item">2 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Mia</div>
+          <div className="witness-item">3 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Sophia</div>
+          <div className="witness-item">4 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>David</div>
+          <div className="witness-item">5 <div className="profile-pic1"> <img src={"profile-icon.svg"} alt="icon" /> </div>Luca</div>
+        </div>
+        <div>
+                    <a href="/" style={{textDecoration: 'none'}} className="view-all">View All</a>
+        </div>
+      </footer>
+{/* end of bottom bar */}
+
+
+
+
+
+
+
+
+    </div>
+   
+  );
+}
+
+{/* TESTING FOR GITHUB */}
+
+export default App;
